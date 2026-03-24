f28cf6a0c5120b9e4480e8ad7424e6bb68eb8b78
Olaf Jaworski <s33536@pjwstk.edu.pl>
2026-03-24 20:57:45 +0100

    Commit class Projector

---
+++ apbd-cw3-s33536/Mod/Sprzet/Projector.cs
@@ -0,0 +1,13 @@
+﻿namespace apbd_cw3_s33536.Mod.Sprzet;
+
+public class Projector : Item
+{
+    public string Res { get; set; }
+    public int Lumen { get; set; }
+    
+    public Projector(string name, string res, int lumen) : base(name)
+    {
+        Res = res;
+        Lumen = lumen;
+    }
+}

128d0d59a7db7369ecf603deea0a8b240b443f65
Olaf Jaworski <s33536@pjwstk.edu.pl>
2026-03-24 21:19:40 +0100

    Commit merge User and Equipment

---
+++ apbd-cw3-s33536/Mod/Uzytkownicy/Pracownik.cs
@@ -0,0 +1,13 @@
+﻿namespace apbd_cw3_s33536.Mod.Uzytkownicy;
+
+public class Pracownik : User
+{
+    public string Dept { get; set; }
+
+    public override int maxWypo => 5;
+
+    public Pracownik(string fname, string lname, string dept) : base(fname, lname)
+    {
+        Dept = dept;
+    }
+}

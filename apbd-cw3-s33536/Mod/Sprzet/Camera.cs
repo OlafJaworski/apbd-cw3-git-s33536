375ecbf80fed5fe4c2c1632ecdca6c09372d0e5a
Olaf Jaworski <s33536@pjwstk.edu.pl>
2026-03-24 20:59:47 +0100

    Commit class Camera

---
+++ apbd-cw3-s33536/Mod/Sprzet/Camera.cs
@@ -0,0 +1,13 @@
+﻿namespace apbd_cw3_s33536.Mod.Sprzet;
+
+public class Camera : Item
+{
+    public int Megapixel { get; set; }
+    public bool maObiektyw { get; set; }
+
+    public Camera(string name, int megapixel, bool maobiektyw) : base(name)
+    {
+        Megapixel = megapixel;
+        maObiektyw = maobiektyw;
+    }
+}

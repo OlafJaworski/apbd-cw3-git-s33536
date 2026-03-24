e2ed853307f9a922769c821210f6940a7ff9e388
Olaf Jaworski <s33536@pjwstk.edu.pl>
2026-03-24 20:52:44 +0100

    Commit class Item

---
+++ apbd-cw3-s33536/Mod/Sprzet/Item.cs
@@ -0,0 +1,13 @@
+﻿namespace apbd_cw3_s33536.Mod.Sprzet;
+
+public abstract class Item
+{
+    public Guid Id { get;} = Guid.NewGuid();
+    public string Name { get; set; }
+    public bool Dostepny { get; set; } = true;
+
+    protected Item(string name)
+        {
+        Name = name;
+        }
+}

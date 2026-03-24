fcaa2bb59a6d3bea2e75231192a0f3363abb0bb4
Olaf Jaworski <s33536@pjwstk.edu.pl>
2026-03-24 20:35:53 +0100

    Commit class Student

---
+++ apbd-cw3-s33536/Mod/Uzytkownicy/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace apbd_cw3_s33536.Mod.Uzytkownicy;
+
+public class Student : User
+{
+    public override int maxWypo => 2;
+    public string studentId { get; set; }
+    
+    public Student(string fname, string lname, string id) : base(fname, lname)
+    {
+        studentId = id;
+        
+    }
+}
--- apbd-cw3-s33536/Mod/Uzytkownicy/User.cs
+++ apbd-cw3-s33536/Mod/Uzytkownicy/User.cs
@@ -5,7 +5,6 @@ public abstract class  User
     public Guid Id { get; } = Guid.NewGuid();
     public string fName { get; set; } 
     public string lName { get; set; }
-    
     public abstract int maxWypo { get; }
 
     protected User(string fname, string lname)

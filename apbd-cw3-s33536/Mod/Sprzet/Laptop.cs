283ba1aaf129281029db7539a7ca536e35025a22
Olaf Jaworski <s33536@pjwstk.edu.pl>
2026-03-24 20:55:51 +0100

    Commit class Laptop

---
+++ apbd-cw3-s33536/Mod/Sprzet/Laptop.cs
@@ -0,0 +1,13 @@
+﻿namespace apbd_cw3_s33536.Mod.Sprzet;
+
+public class Laptop : Item
+{
+    public int ramGB { get; set; }
+    public string Procesor { get; set; }
+
+    public Laptop(string name, int ramgb, string procesor) : base(name)
+    {
+        ramGB = ramgb;
+        Procesor = procesor;
+    }
+}
